8e38e8c5d2dad84a42b949d795aa20f10d109c49
Paul Gierz <pgierz@awi.de>
2016-07-27 10:27:58 +0200

    made new run tool more complete

--- scripts/new_pism_standalone.sh
+++ scripts/new_pism_standalone.sh
@@ -5,8 +5,12 @@ INDIR=input
 OUTDIR=output
 RESDIR=restart
 SCRIPTDIR=scripts
+WORKDIR=work
+BINDIR=bin
 
-for d in $INDIR $OUTDIR $RESDIR $SCRIPTDIR
+for d in $INDIR $OUTDIR $RESDIR $SCRIPTDIR $WORKDIR $BINDIR
 do
     mkdir -pv $expid/$d
 done
+
+cat ${HOME}/palmod_pism_standalone/scripts/run_script_template.sh |sed s+@EXPID@+"${expid}"+g > $expid/${SCRIPTDIR}/${expid}.run
