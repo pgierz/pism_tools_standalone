155888700bc537b77151835a6148d3d0010e3463
Paul Gierz <pgierz@awi.de>
2016-09-09 13:41:16 +0200

    Update from git_push_logout script

---
+++ scripts/pism_input_from_gcm.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+# This script is designed to take input from a GCM (e.g. COSMOS) and
+# generate the correct input fields for a PISM simulation.
+#
+# For help, supply the flag -h
+#
+# Paul J. Gierz, Fri Sep  9 13:22:00 2016
+
